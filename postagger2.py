80f796f234441cde4b67904d1fee572156b1a1a3
Leo Marko Englund <Leo Marko Englund>
2016-10-03 15:52:00 +0200

    trying to use more features in the tagger

---
+++ postagger2.py
@@ -0,0 +1,109 @@
+#importer
+from perceptron2 import train
+import os
+# -*- coding: utf-8 -*-
+
+
+#----------------------------------------------------------------------------------------
+# klassen TagPredictor med sina funktioner nedan
+class TagPredictor():
+    def __init__(self, tupler):
+        #datasortering, gör lista med särdrag för varje ord,
+        #och lista med taggar som matchar den:
+        ordlista = [t[0] for t in tupler]
+        affix = self.skapa_affixlista(ordlista)
+        taggar = [t[1] for t in tupler]
+
+        #maskininlärning:
+        examples = list(zip(affix,taggar))
+        self.apmodel = train(5, examples)
+
+    def skapa_affixlista(self, ordlista):
+        #skapar vektorutrymme inkl. en vektor för varje affix
+        #returnerar lista med uppslagslistor
+        affixlista = []
+        for o in ordlista:
+            affix = ta_fram_affix(o)
+            affixlista.append(affix) #för varje ord läggs ett set affix till
+        return affixlista
+
+    def predict(self, ordlista):
+        #förutser taggarna som varje ord borde få utifrån den tränade LogisticRegression-estimatorn
+        affixlista = self.skapa_affixlista(ordlista)
+        tags = [self.apmodel.predict(a) for a in affixlista]
+        return tags
+
+
+#----------------------------------------------------------------------------------------------------
+#globala funktioner
+def read_data(fil):
+    #returnerar en lista med tupler
+    tupler = []    
+    with open(fil, 'r') as f:
+        lines = [rad for rad in f if rad != '\n'] #ignorerar blankrader
+        #ord och ordklass tas fram från varje rad, vilket är 2a respektive 4e elementet:
+        for line in lines:
+            data = line.split()[1:4:2]
+            if len(data[0]) > 3: #orden måste vara minst 3 bokstäver långa
+                tupler.append((data[0],data[1])) #varje par av ord och ordklass bildar en tupel
+    return tupler
+
+
+def get_features(prevw,word,nextw):
+"""
+adds all affixes up to 5 characters long, assuming the root is at least 2 characters
+prefixes marked with -p- after
+adds suffixes up to 3 characters long from surrounding words, marked with -prev- and -next-
+if the word is 2 characters or less, the whole word is added as a feature (instead of affixes)
+this feature is marked -w- after
+returns a set of features
+"""
+    def _add_features(word, max_length, mark=''):
+        if word:
+            if len(word) < 3:
+                features.add(mark + word + '-w-')
+            for i in range(1, len(word) - 1):
+                if i <= max_length:
+                    features.add(mark + word[-i:]) #suffix
+                    if mark == '':
+                        features.add(mark + word[:i] + '-p-') #prefix, only added for main word
+                             
+    features = set()
+    _add_features(word, 5)
+    _add_features(prevw, 3, '-prev-')
+    _add_features(nextw, 3, '-next-')
+    return features
+
+
+#-------------------------------------------------------------------------------------------
+#main-delen av programmet, testar algoritmen
+def main(traeningsfil, testfil):
+    #läser in träningsdatan och skapar TagPred.-objekt som har tränats baserat på den datan
+    traeningsdata = read_data(traeningsfil)
+    tagpredictor = TagPredictor(traeningsdata)
+
+    #taggarna för testdatat förutsägs här
+    testdata = read_data(testfil)
+    ordlista = [tupel[0] for tupel in testdata]
+    taggar = tagpredictor.predict(ordlista)
+    facit_taggar = [tupel[1] for tupel in testdata]
+    
+
+    #här stäms det av hur många taggar blev korrekta och skriver ut resultaten för det
+    korrekt = 0
+    fel = 0
+    for i in range(len(ordlista)):
+        if taggar[i] == facit_taggar[i]:
+            korrekt += 1
+        else:
+            fel += 1
+    procent_korrekt = round(korrekt / len(ordlista) * 100)
+    
+    print("Antal korrekt taggade ord: " + str(korrekt) + "\n" +
+          "Antal feltaggade ord: " + str(fel) + "\n" +
+          "Andel korrekt taggade ord: " + str(procent_korrekt) + "%")
+
+#testkörning (svenska)   
+traening = os.curdir+"\sv-universal-train.conll"
+test = os.curdir+"\sv-universal-test.conll"
+main(traening, test)
