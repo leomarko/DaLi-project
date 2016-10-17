43b4d96c5be366aebc2b2d963a66f2e424568bf7
Leo Marko Englund <Leo Marko Englund>
2016-10-17 17:06:15 +0200

    back to earlier version, plus some notes on improvement

--- postagger2.py
+++ postagger2.py
@@ -1,14 +1,14 @@
 #importer
 from perceptron2 import AveragedPerceptron, train as aptrain
-from compoundabilitydict import CompableDict, word_to_tag_dict
 import os
 import pickle
-import re
 # -*- coding: utf-8 -*-
 
 """
 notes:
-End or start of sentence should be added as a features
+- Add question mark
+- Capitalization or not of (only) non initial word
+- When testing, compile a list of the mistagged words and/or tags
 """
 
 #----------------------------------------------------------------------------------------
@@ -77,36 +77,28 @@ def make_featurelist(s):
             n += 1
         return f_list
 
-def get_features(word, prevw=None, nextw=None, prevtags=None):
+def get_features(word, prevw=None, nextw=None):
     """
     adds all affixes up to 5 characters long, assuming the root is at least 2 characters
     prefixes marked with -p- after
     adds suffixes up to 3 characters long from surrounding words, marked with -prev- and -next-
-    adds the whole word is added as a feature marked -w-
-    adds the correct/predicted tags for the 2 previous words (test also with predicted only)
+    the whole word is added as a feature, marked -w- after
     returns a set of features
     """
-    def _add_features(word, max_length, mark='', tag=None):
-        features.add(mark + word + '-w-')
-        for i in range(1, len(word) - 1):
-            if i > max_length:
-                break
-            if mark == '':
-                features.add(mark + word[:i] + '-p-') #prefix, only added for main word
-            features.add(mark + word[-i:] + '-s-') #suffix
-        if tag:
-            features.add(mark + word[-i:] + '-t-')
+    def _add_features(word, max_length, mark=''):
+        if word:
+            features.add(mark + word + '-w-')
+            for i in range(1, len(word) - 1):
+                if i > max_length:
+                    break
+                if mark == '':
+                    features.add(mark + word[:i] + '-p-') #prefix, only added for main word
+                features.add(mark + word[-i:]) #suffix
                 
     features = set()
     _add_features(word, 5)
-    if prevw:
-        _add_features(prevw, 3, '-prev-')
-    else:
-        features.add('-first-')
-    if nextw:
-        _add_features(nextw, 3, '-next-')
-    else:
-        features.add('-last-')
+    _add_features(prevw, 3, '-prev-')
+    _add_features(nextw, 3, '-next-')
     return features
 
 
@@ -139,7 +131,7 @@ def test(training_file, test_file, nr_iters):
 
 #---------------------------------------------------------------------------------
 #training and saving a model to use with the tagger
-def save_apmodel(training_file,savedir, nr_iters):
+def save_apmodel(training_file,nr_iters,savedir):
     sentences, tags = read_data(training_file)
     tagpredictor = TagPredictor()
     tagpredictor.train(sentences,tags,nr_iters)
@@ -148,10 +140,10 @@ def save_apmodel(training_file,savedir, nr_iters):
 
 #---------------------------------------------------------------------------------
 def main():
-    training = os.curdir+"\sv-universal-train.conll"
-    testing = os.curdir+"\sv-universal-test.conll"
+    training = "sv-universal-train.conll"
+    testing = "sv-universal-test.conll"
     savedir = 'apmodel.p'
-    mode = int(input('Train and test, train and save, or check compundability?\n Input 1, 2 or 3\n'))
+    mode = int(input('Train and test, or train and save? Input 1 or 2\n'))
     nr_iters = int(input('Number of iterations for training:\n'))
     if mode == 1:
         test(training, testing, nr_iters)
@@ -159,16 +151,6 @@ def main():
         savedir = 'apmodel_' + input('version name:\n') + '.p'
         save_apmodel(training, savedir, nr_iters)
         print('Saved')
-    elif mode == 3:
-        sentences = read_data(training)
-        features = make_featurelist(sentences)
-        cpb = CompableDict(features)
-        cpb.show()
-        print('prefixes:')
-        print(word_to_tag_dict(cpb.prefixscores))
-        print('suffixes:')
-        print(word_to_tag_dict(cpb.suffixscores))
-        
 
 if __name__ == '__main__':
     main()
