cb9097a530f3e6a116f690797079b59af1904263
Leo Marko Englund <Leo Marko Englund>
2016-10-24 14:36:55 +0200

    cleaner and better command line options

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 from postagger2 import TagPredictor
 from nltk.tokenize import sent_tokenize, word_tokenize
 from compounddetector import CompoundDetector
+from optparse import OptionParser
 import codecs, sys, getopt
 
 def load_postagger(path, ambiguous, min_percent):
     postagger = TagPredictor(loadpath=path,ambiguous=ambiguous, min_percent=min_percent)
     return postagger
 
-def test_tagging(examplefile):
-    with codecs.open(examplefile,'r', encoding='utf-8') as file:
-        sentences = sent_tokenize(file.read(),language='swedish')
-    output = [postagger.tokenize_tag(s) for s in sentences]
-    for i in range(40):
-        print(output[i])
-        print('\n')
-
+#single sentences
 def detect_compounds(sentence, flexible=False):
+    #returns the indices for the first words of detected compounds
     tags = [t[1] for t in postagger.tokenize_tag(sentence)]
     if flexible:
         return cpd.flexibledetect(tagsets)
     return cpd.detect(tags)
 
+#whole texts
 def compound_errors(filename, returnstring=False):
+    #returns a list of line numbers and detected compounds,
+    #either as tuples with 3 elements or as a string (if returnstring=True)
     with codecs.open(filename,'r', encoding='utf-8') as file:
         raw = file.read()
         
@@ -57,37 +55,33 @@ def compound_errors(filename, returnstring=False):
         output = ['line '+str(t[0])+': '+' '.join(t[1:3]) for t in output]
     return output    
 
+#-------------------------------------------Run
 if __name__ == '__main__':
+    parser = OptionParser(usage='usage: %prog [options] textfile')
+    parser.add_option('-f', '--filename', dest='outfile',
+                      help='specify filename for output file')
+    parser.add_option('-p', '--minpercent', dest='percent', type=int, default=75,
+                      help='set minimum percent of max score for POS-tags ("unambiguity level")')
+    parser.add_option('-t', '--test', dest='test',
+                      help='evaluate detections with testfile')
+    parser.add_option('--apmodel',dest='apmodel', default='apmodel_suc3iter.p',
+                      help='specify pickled averaged perceptron model to use')
+    opts, args = parser.parse_args()
     ambiguous = True
-    min_percent = 75
-    test = False
-    try:
-        opts, args = getopt.getopt(sys.argv[1:],'ht:p:')
-        for opt, arg in opts:
-            if opt == '-h':
-                print('main.py text.txt -t <testfile> -p <min_percent>')
-                sys.exit()
-            elif opt == '-t':
-                test = True
-                print(arg)
-                testfile = arg #correct answers
-            elif opt == '-p':
-                assert isinstance(arg,int) and arg > 0
-                if arg > 99:
-                    ambiguous = False
-                else:
-                    min_percent = arg
-    except getopt.GetoptError:
-        print('getopterror')
-    postagger = load_postagger('apmodel_suc3iter.p', ambiguous, min_percent)
+    if opts.percent > 99:
+        ambiguous = False
+
+    postagger = load_postagger(opts.apmodel, ambiguous, opts.percent)
     cpd = CompoundDetector()
-    if not test:
+    
+    if not opts.test:
         with open(args[0][:-4]+'_detections.txt', 'w', encoding='utf-8') as outfile:
             for error in compound_errors(args[0],returnstring=True):
                 outfile.write(error)
                 outfile.write('\n')
+                
     else:
-        with open(testfile, 'r', encoding='utf-8') as correct:
+        with open(opts.test, 'r', encoding='utf-8') as correct:
             answers = [line.split() for line in correct]
         for line in answers:
             try:
@@ -95,11 +89,10 @@ if __name__ == '__main__':
             except(ValueError):
                 line[0] = int(line[0][1:])
         answers = list(map(tuple,answers))
-        detetions = compound_errors(args[0])
+        detections = compound_errors(args[0])
         accurate = len([d for d in detections if d in answers])
         missed = len([a for a in answers if a not in detections])
         precision = round(accurate/len(detections), 3)
         recall = round(accurate/(accurate+missed), 3)
         f_score = round((2*precision*recall) / (precision + recall), 3)
         print('Precision: {}, Recall: {}, F-score: {}'.format(precision,recall,f_score))        
-            
